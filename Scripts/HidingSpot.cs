3929fc8924a8e7b1febdb31aaade24bad2457efe
Jason Anderson <moosnake1@bigpond.com>
2021-03-03 20:28:10 +1030

    Scripts

---
+++ Scripts/HidingSpot.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HidingSpot : MonoBehaviour
+{
+    public bool Occupied = false;
+    public GameObject enemyPrefab;
+    public Transform SpawnPosition;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+       
+    }
+
+    private void OnTriggerEnter(Collider coll)
+    {
+        if (coll.gameObject.tag == "Enemy")
+        {
+            if (Occupied == false)
+            {
+                Occupied = true;
+                Destroy(coll.gameObject);
+            }
+        }
+       
+        if (coll.gameObject.tag == "Player")
+        {
+            if (Occupied == true)
+            {
+               StartCoroutine(Found());
+            }
+        }
+    }
+
+
+    IEnumerator Found()
+    {
+        Instantiate(enemyPrefab, SpawnPosition.transform.position, SpawnPosition.transform.rotation);
+        yield return new WaitForSeconds(3f);
+        Occupied = false;
+    }
+
+
+}
