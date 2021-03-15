7006ed8fdbd69458bf38f06ed7340647c7fc0488
Daniel <danielsaulmckearney@gmail.com>
2021-03-15 20:17:35 +1030

    W.I.P.
    Added hiding, objective and enemy prefabs + scripts

---
+++ Hide and seek level greybox/Assets/Scripts/Objective Scripts/Objectivebar2.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Objectivebar2 : MonoBehaviour
+{
+    public float objectivestarting2 = 0f;
+    public float currentprogress2;
+    public float maxprogress2 = 100;
+    public GameObject Objective2;
+    public Vector3 Done2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentprogress2 = objectivestarting2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentprogress2 >= maxprogress2)
+        {
+        Objective2.transform.position = Done2;
+        }
+    }
+
+   private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            InvokeRepeating("Objectivefill", 1.0f, 2.0f);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            CancelInvoke("Objectivefill2");
+        }
+    }
+
+    public void Objectivefill2()
+    {
+        currentprogress2 += 10;
+    }
+
+}
---
+++ Hide and seek level greybox/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    #region Singleton
+    public static PlayerManager instance;
+
+    void Awake()
+    {
+        instance = this;
+    }
+    #endregion 
+
+    public GameObject Player;
+    public GameObject Hiding;
+    public GameObject Objective;
+    public GameObject Objective1;
+    public GameObject Objective2;
+    public GameObject Door;
+    public float Enemiesdead;
+    public float objectivesdone;
+
+    void Start()
+    {
+        Enemiesdead = 0f;
+    }
+
+    void Update()
+    {
+        if (Enemiesdead == 5)
+        {
+            //increase ai speed higher
+        }
+
+        if (Enemiesdead == 10)
+        {
+            //player wins
+        }
+
+        if (objectivesdone == 10)
+        {
+            //player loses
+        }
+    }
+}
