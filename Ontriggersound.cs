079bfa9eee7b552af8112408f662b53aaf28b8e4
JasonAnderson615 <79190613+JasonAnderson615@users.noreply.github.com>
2021-04-21 19:58:59 +0930

    Add files via upload
    basic sound trigger

---
+++ Ontriggersound.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class Ontriggersound : MonoBehaviour
+{
+    public AudioSource sound;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Object")
+        {
+            sound.Play();
+        }
+    }
+
+}
